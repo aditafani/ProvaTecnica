6162dac171527022264a99c8f51de23284de097c
Adi Tafani <aditafani@MBP-di-Adi.station>
2022-05-01 15:07:06 +0200

    prova 9

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    //res.json({ message: "Hello from server!" });
+    res.json({ message: req });
   });
   
 app.listen(PORT, () => {
