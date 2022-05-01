d35a76ffe6bccbac6776950b41121d3f44f8ff8b
Adi Tafani <aditafani@MBP-di-Adi.station>
2022-05-01 14:53:46 +0200

    Prova4?

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
